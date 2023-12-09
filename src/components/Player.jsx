3b9321fe37a150e8dbce3632442d2daf76cfcc4b
Saurabh-Rana17 <saurabhrana200317@gmail.com>
2023-12-09 10:51:14 +0530

    btn edit feature added

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,20 +2,25 @@ import React, { useState } from "react";
 
 export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
+
   function handleEditClick() {
-    setIsEditing(true);
+    setIsEditing((editing) => !editing);
   }
+  let btnCaption = "Edit";
+
   let playerName = <span className="player-name">{name}</span>;
   if (isEditing) {
-    playerName = <input type="text" required />;
+    playerName = <input type="text" value={name} required />;
+    btnCaption = "Save";
   }
+
   return (
     <li>
       <span className="player">
         {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>edit</button>
+      <button onClick={handleEditClick}>{btnCaption}</button>
     </li>
   );
 }
